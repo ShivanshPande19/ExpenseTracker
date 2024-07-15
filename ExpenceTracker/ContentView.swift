aefcd44fcb3febb2dab0248e1762d1437c514d9a
ShivanshPande19 <pshivanshofficial@gmail.com>
2024-07-15 13:25:37 +0530

    Initial Commit

---
+++ ExpenceTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenceTracker
+//
+//  Created by SHIVANSH PANDE on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenceTracker/ExpenceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenceTrackerApp.swift
+//  ExpenceTracker
+//
+//  Created by SHIVANSH PANDE on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
